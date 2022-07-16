03eece6fcb7443dc2a89541bf9e75ecf3276d9c9
Morteza <morteza.khosravinejad@gmail.com>
2022-07-16 14:21:24 +0300

    adding database

---
+++ app/src/main/java/com/moryzaky/chalkboard/data/source/local/ChalkBoardDatabase.kt
@@ -0,0 +1,27 @@
+package com.moryzaky.chalkboard.data.source.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.moryzaky.chalkboard.BuildConfig
+import com.moryzaky.chalkboard.data.model.PersonDataModel
+import com.moryzaky.chalkboard.data.source.local.dao.PersonDao
+
+/**
+ * Created by Morteza Khosravinejad on 16/07/2022.
+ */
+
+@Database(
+    entities = [
+        PersonDataModel::class
+    ],
+    version = BuildConfig.VERSION_CODE,
+    exportSchema = false
+)
+abstract class ChalkBoardDatabase : RoomDatabase() {
+    companion object {
+        const val DB_NAME = "chalkboard.db"
+    }
+
+    abstract val personDao: PersonDao
+}
