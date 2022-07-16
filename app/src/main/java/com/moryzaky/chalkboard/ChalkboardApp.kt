2b590810f52f7cfd012fc5accdb1da0a24f4ad49
Morteza <morteza.khosravinejad@gmail.com>
2022-07-16 16:05:18 +0300

    adding application class

---
+++ app/src/main/java/com/moryzaky/chalkboard/ChalkboardApp.kt
@@ -0,0 +1,12 @@
+package com.moryzaky.chalkboard
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Morteza Khosravinejad on 16/07/2022.
+ */
+
+@HiltAndroidApp
+class ChalkboardApp: Application() {
+}
