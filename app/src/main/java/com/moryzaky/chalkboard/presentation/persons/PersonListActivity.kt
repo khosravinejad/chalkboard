e3d3d2d7c89ee8b5aef7d03ae1b1f9a5482075c6
Morteza <morteza.khosravinejad@gmail.com>
2022-07-16 16:04:28 +0300

    add person list activity design

--- app/src/main/java/com/moryzaky/chalkboard/presentation/persons/PersonListActivity.kt
+++ app/src/main/java/com/moryzaky/chalkboard/presentation/persons/PersonListActivity.kt
@@ -1,17 +1,48 @@
 package com.moryzaky.chalkboard.presentation.persons
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.moryzaky.chalkboard.R
 import com.moryzaky.chalkboard.databinding.ActivityPersonListBinding
+import com.moryzaky.chalkboard.domain.model.PersonDomainModel
 import com.moryzaky.chalkboard.presentation.base.BaseActivity
+import com.moryzaky.chalkboard.presentation.base.OnItemClickListener
+import com.moryzaky.chalkboard.presentation.persons.recyclerview.PersonsAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class PersonListActivity :
-    BaseActivity<ActivityPersonListBinding, PersonListViewModel>(PersonListViewModel::class.java) {
+    BaseActivity<ActivityPersonListBinding, PersonListViewModel>(PersonListViewModel::class.java),
+    OnItemClickListener<PersonDomainModel> {
+
+    @Inject
+    lateinit var personsAdapter: PersonsAdapter
+
+    private val layoutManager by lazy {
+        LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+
     override fun initializeUI(savedInstanceState: Bundle?) {
-        TODO("Not yet implemented")
+
+        binding.recyclerview.apply {
+            adapter = personsAdapter
+            layoutManager = this@PersonListActivity.layoutManager
+        }
+
+        observe()
+        viewModel.loadPersons()
+        personsAdapter.setOnItemClickListener(this)
+    }
+
+    private fun observe() {
+        viewModel.persons.observe(this) {
+            personsAdapter.submitData(lifecycle, it)
+        }
     }
 
     override fun getLayoutId() = R.layout.activity_person_list
+    override fun onItemClick(item: PersonDomainModel) {
+
+    }
 }
